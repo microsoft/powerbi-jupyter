19a3c91d56d46301edc89a7851fc5410c481712b
Or Shemesh <orshemes@microsoft.com>
2022-09-28 17:57:55 +0300

    remove pipelines folder

--- .pipelines/test.ps1
+++
@@ -1,9 +0,0 @@
-$exitCode = 0;
-
-# Run python kernel test scripts
-Write-Host "start: pytest powerbiclient"
-& python -m pytest powerbiclient --cov -v
-Write-Host "done: pytest powerbiclient"
-$exitCode += $LASTEXITCODE;
-
-exit $exitCode;
