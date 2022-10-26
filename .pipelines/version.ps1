4d1850905dbc2385f0d4b19aba4cd74dc557ec0b
Kotana Sai <v-kotanasai@microsoft.com>
2022-10-26 12:06:02 +0530

    Aligned yml file

--- .pipelines/version.ps1
+++ .pipelines/version.ps1
@@ -2,14 +2,13 @@ try {
     # package.json is in root folder, while version.ps1 runs in .pipelines folder.
     # we will use the npm version as it will be used to set the tag as latest in npm
     $version = (Get-Content "package.json") -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
-    $buildNumber = "$version"
-  
+    $revision = $env:CDP_DEFINITION_BUILD_COUNT
+    $buildNumber = "$version.$revision"
+
     Write-Host "Build Number is" $buildNumber
-  
+
     # This will allow you to use it from env var in later steps of the same phase
-    Write-Host "##vso[task.setvariable variable=Version]${version}"
     Write-Host "##vso[task.setvariable variable=CustomBuildNumber]${buildNumber}"
-    Write-Host "##vso[build.updatebuildnumber]${buildNumber}"                         # This will update build number on your build
   }
   catch {
     Write-Error $_.Exception
